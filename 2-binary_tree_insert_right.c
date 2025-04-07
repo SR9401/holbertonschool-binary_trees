e1b29472fbc7c48703c72583443c9c6d8637d18f
root <shakibrojas@icoud.com>
2025-04-07 12:01:35 +0000

    push task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+*binary_tree_insert_right - fonction qui ajoute un noued a droite du noeud
+*
+*@parent: noeud ou la fonction agit
+*
+*@value: valeur du nouveau noeud
+*
+*Return: adresse du nouveau noeud ou null si echec
+**/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+
+binary_tree_t *n_noeud;
+
+	n_noeud = malloc(sizeof(binary_tree_t));
+
+	if (n_noeud == NULL)
+	{
+		return (NULL);
+	}
+
+	n_noeud->n = value;
+	n_noeud->left = NULL;
+	n_noeud->right = NULL;
+	n_noeud->parent = parent;
+
+	if (parent->right != NULL)
+	{
+	n_noeud->right = parent->right;
+	parent->right->parent = n_noeud;
+	}
+	parent->right = n_noeud;
+
+return (n_noeud);
+}
