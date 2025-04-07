2011437989ffa68f48d86433311fd4baf3e98fde
root <shakibrojas@icoud.com>
2025-04-07 12:17:34 +0000

    push t 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+*binary_tree_is_leaf - fonction qui verifie si un noeud est une feuille
+*
+*@node: noeud a verifier
+*
+*Return: 1 si vrai 0 si faux
+**/
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+
+	if (node->right == NULL && node->left == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
