e7f9e3c486609c5e7e6133e7ac9c7cc724574f7d
root <shakibrojas@icoud.com>
2025-04-07 10:17:21 +0000

    correction t0 betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
+*binary_tree_node - fonction pour creer un noued enfant
 *
+*@parent: pointeur sur parent
 *
+*@value: valeur du nouveau noeud enfant
 *
-*
-*
-*
-*
+*Return: adresse du nouveau noeud ou null si echec
 **/
 
 
