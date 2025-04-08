4c82d9b45eb8f4711b0f2479c0e3baadd46f7102
root <shakibrojas@icloud.com>
2025-04-08 08:30:08 +0000

    push t8

--- binary_trees.h
+++ binary_trees.h
@@ -25,7 +25,7 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
