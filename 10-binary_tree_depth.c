2b0dadb5cfde88ba64d61cc9bf5910a56e62b9f1
root <shakibrojas@icloud.com>
2025-04-09 14:29:03 +0000

    correction betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,12 +1,11 @@
 #include "binary_trees.h"
 
 /**
+*binary_tree_depth - fonction qui mesure la profondeur d'un noeud
 *
+*@tree: pointeur sur le noeud a mesurer
 *
-*
-*
-*
-*
+*Return: profondeur du noeud
 **/
 
 size_t binary_tree_depth(const binary_tree_t *tree)
@@ -25,6 +24,6 @@ while (tree != NULL)
 tree = tree->parent;
 n++;
 }
-return (n-1);
+return (n - 1);
 
 }
