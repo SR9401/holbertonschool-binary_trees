f5afe930327876bf781bfc714675a55c85b169c0
SR9401 <shakibrojas@icloud.com>
2025-04-09 16:13:03 +0200

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -25,9 +25,6 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-int binary_tree_balance(const binary_tree_t *tree);
-size_t binary_tree_leaves(const binary_tree_t *tree);
-size_t binary_tree_depth(const binary_tree_t *tree);
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -36,6 +33,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-int binary_tree_is_perfect(const binary_tree_t *tree);
-binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif /* _BINARY_TREES_H_ */
