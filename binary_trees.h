fb2987d841dad9f0a60d480c29c289c6cff5d813
root <shakibrojas@icloud.com>
2025-04-09 09:09:48 +0000

    t 12

--- binary_trees.h
+++ binary_trees.h
@@ -25,6 +25,7 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
